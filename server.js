568d3e5f50c8f06cd702d0f6fe18109738ee84a7
TeslaNicola <rezvih12@gmail.com>
2021-11-19 21:10:45 +0200

    hi

---
+++ server.js
@@ -0,0 +1,16 @@
+const net = require('net');
+const port = 3036;
+const server = net.createServer((connection) => {
+  console.log('client connected');
+
+  connection.on('end', () => {
+    console.log('client disconnected');
+  });
+  connection.write('Hello World!\r\n');
+  connection.write('Hello World 2');
+  connection.pipe(connection);
+});
+
+server.listen(port, () => {
+  console.log(`TCP server is listening to ${port} port`);
+}); 
